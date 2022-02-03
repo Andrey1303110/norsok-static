def46e161924e9e1eef50563ea1fc9fbb9475295
andrey1303110 <andrey1303110@gmail.com>
2022-02-03 18:54:39 +0200

    add arrows for main slider

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     $('.main-section').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        speed: 650,
+        speed: 1000,
         arrows: true,
         dots: false,
         infinite: false,
-        prevArrow: $('.prev'),
-        nextArrow: $('.next'),
+        prevArrow: $('.arrow.arrow_left'),
+        nextArrow: $('.arrow.arrow_right'),
     })
 })
