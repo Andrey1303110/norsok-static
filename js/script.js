b71a713a8dad86c6e83c9fa123dfb521dec670cd
andrey1303110 <andrey1303110@gmail.com>
2022-02-04 15:00:28 +0200

    add functions for video

--- js/script.js
+++ js/script.js
@@ -10,11 +10,24 @@ $(document).ready(function(){
         nextArrow: $('.arrow.arrow_right'),
     })
 
+    $('.arrow.arrow_right')[$('.arrow.arrow_right').length-1].disabled = true
+
     $('button.play').click(function(){
         $('.video-section').css('left', '0vw');
+        $('video')[0].src = 'media/video/'+ $(this)[0].dataset.name +'.mp4';
+        $('video')[0].poster = 'media/images/poster_'+ $(this)[0].dataset.name +'.png';
     })
+
     $('video').on('ended', function(){
         $('.video-section').css('left', '100vw');
         $(this)[0].currentTime = '0';
+    })
+    $('.video-section .close').click(function(){
+        $(this).css('transform', 'rotate(360deg)');
+        $(this).on('transitionend webkitTransitionEnd oTransitionEnd', function () {
+            $('.video-section').css('left', '100vw');
+            $(this)[0].currentTime = '0';
+            $('.video-section .close').css('transform', 'rotate(0deg)');
+        });
     })
 })
